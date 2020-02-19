1fb233a97bb578a1fbc47baa28afff99d6fae27a
Maltus Vandrel <matgoben@gmail.com>
2020-02-19 17:40:36 -0300

    re do

--- src/app/data/demos.ts
+++
@@ -1,8 +0,0 @@
-import { Place } from "../model/system/place";
-import { Game } from "../model/system/game";
-import { gamesMap } from "./games";
-import { Demo } from "../model/development/demo";
-
-let demos:Demo[] = [];
-demos.push(new Demo("The Void","/place","DEMOS_VOID"));
-export let demoList=demos;
--- src/app/model/system/activity.ts
+++
@@ -1,9 +0,0 @@
-import { GameDialog } from "./game-dialog";
-import { Game } from "./game";
-
-export class Activity{
-    private _action:(game:Game)=>GameDialog;
-    private _criterea:(game:Game)=>GameDialog;
-    private _running:Boolean;
-       
-}
