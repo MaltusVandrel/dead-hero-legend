a6941c54bb6b8cf15fcafe2cf15df53826dc0169
Maltus Vandrel <matgoben@gmail.com>
2020-02-20 09:19:08 -0300

    jquery and stuff

---
+++ game/helpers/general.js
@@ -0,0 +1,4 @@
+
+
+window['$El']=(tag)=>{return $(document.createElement(tag));};
+window['Text']=(text)=>{return document.createTextNode(text)};
---
+++ game/main.js
@@ -0,0 +1,5 @@
+
+$('#text').html('');
+var p = $El('p');
+p.text('Esse é um texto simples adicionado em uma div');
+$('#text').append(p);
