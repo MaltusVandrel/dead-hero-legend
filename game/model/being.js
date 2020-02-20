65fe61730e5910ab535f6cf85ddf7fa63543bdc3
Maltus Vandrel <matgoben@gmail.com>
2020-02-20 17:51:18 -0300

    all

---
+++ game/model/being.js
@@ -0,0 +1,21 @@
+class Being {
+    
+    str=0;
+    dex=0;
+    con=0;
+    int=0;
+    sab=0;
+    car=0;
+    wil=0;
+    luk=0;
+    
+    constructor(obj) {
+      
+    }
+
+    tackle(){
+        this
+    }
+
+
+  }
