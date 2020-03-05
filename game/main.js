2d7dab2284da6c80844078e0547898af429e7732
Maltus Vandrel <matgoben@gmail.com>
2020-03-05 14:15:09 -0300

    start svg shit

--- game/main.js
+++ game/main.js
@@ -16,3 +16,17 @@ function showMap(){
 function closeMap(){
     $("#root_div_map" ).remove();   
 }
+let svgs={
+    shapes:[{name:'Feminine Face 1',path:'img/face/shape/face-f1.svg'}],
+
+};
+//var rawSvg = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg [...]>"';
+//var draw = SVG('paper');
+//var store = draw.svg(rawSvg);
+
+store.polygon1238.fill('#f06');
+function getSvg(){
+    $.get( "game/widget/map/map-page.html", function( data ) {
+        mapPage=data;
+    });
+}
