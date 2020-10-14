73847c4229e7e24b393a898364071d160120075d
Maltus Vandrel <matgoben@gmail.com>
2020-10-14 13:47:32 -0300

    personality

---
+++ game/data/personality.js
@@ -0,0 +1,75 @@
+var EXPRESSIONS={
+    INNOVATION:{label:"Innovation",description:"Captivate with inventiveness."},
+    PASSION:{label:"Passion",description:"Connect with emotion."},
+    POWER:{label:"Power",description:"Lead with assertiveness."},
+    PRESTIGE:{label:"Prestige",description:"Set higher standards."},
+    TRUST:{label:"Trust",description:"Earn respect by stability."},
+    CUNNING:{label:"Cunning",description:"Communicate meaningfully."},
+    KEEN:{label:"Keen",description:"Prevent with attention"}
+};
+//TODO
+var EXPRESSION_ARCHETYPES={
+    INNOVATION:{
+        INNOVATION:{label:"The Anarchist",description:"Captivate with inventiveness."},
+        PASSION:{label:"The Show Star",description:"Connect with emotion."},
+        POWER:{label:"The Visionary Leader",description:"Lead with assertiveness."},
+        PRESTIGE:{label:"The Trendsetter",description:"Set higher standards."},
+        TRUST:{label:"The Artisan",description:"Earn respect by stability."},
+        CUNNING:{label:"The Provocateur",description:"Communicate meaningfully."},
+        KEEN:{label:"The Quick-starter",description:"Prevent with attention"}
+    },
+    PASSION:{
+        INNOVATION:{label:"Innovation",description:"Captivate with inventiveness."},
+        PASSION:{label:"Passion",description:"Connect with emotion."},
+        POWER:{label:"Power",description:"Lead with assertiveness."},
+        PRESTIGE:{label:"Prestige",description:"Set higher standards."},
+        TRUST:{label:"Trust",description:"Earn respect by stability."},
+        CUNNING:{label:"Cunning",description:"Communicate meaningfully."},
+        KEEN:{label:"Keen",description:"Prevent with attention"}
+    },
+    POWER:{
+        INNOVATION:{label:"Innovation",description:"Captivate with inventiveness."},
+        PASSION:{label:"Passion",description:"Connect with emotion."},
+        POWER:{label:"Power",description:"Lead with assertiveness."},
+        PRESTIGE:{label:"Prestige",description:"Set higher standards."},
+        TRUST:{label:"Trust",description:"Earn respect by stability."},
+        CUNNING:{label:"Cunning",description:"Communicate meaningfully."},
+        KEEN:{label:"Keen",description:"Prevent with attention"}
+    },
+    PRESTIGE:{
+        INNOVATION:{label:"Innovation",description:"Captivate with inventiveness."},
+        PASSION:{label:"Passion",description:"Connect with emotion."},
+        POWER:{label:"Power",description:"Lead with assertiveness."},
+        PRESTIGE:{label:"Prestige",description:"Set higher standards."},
+        TRUST:{label:"Trust",description:"Earn respect by stability."},
+        CUNNING:{label:"Cunning",description:"Communicate meaningfully."},
+        KEEN:{label:"Keen",description:"Prevent with attention"}
+    },
+    TRUST:{
+        INNOVATION:{label:"Innovation",description:"Captivate with inventiveness."},
+        PASSION:{label:"Passion",description:"Connect with emotion."},
+        POWER:{label:"Power",description:"Lead with assertiveness."},
+        PRESTIGE:{label:"Prestige",description:"Set higher standards."},
+        TRUST:{label:"Trust",description:"Earn respect by stability."},
+        CUNNING:{label:"Cunning",description:"Communicate meaningfully."},
+        KEEN:{label:"Keen",description:"Prevent with attention"}
+    },
+    CUNNING:{
+        INNOVATION:{label:"Innovation",description:"Captivate with inventiveness."},
+        PASSION:{label:"Passion",description:"Connect with emotion."},
+        POWER:{label:"Power",description:"Lead with assertiveness."},
+        PRESTIGE:{label:"Prestige",description:"Set higher standards."},
+        TRUST:{label:"Trust",description:"Earn respect by stability."},
+        CUNNING:{label:"Cunning",description:"Communicate meaningfully."},
+        KEEN:{label:"Keen",description:"Prevent with attention"}
+    },
+    KEEN:{
+        INNOVATION:{label:"Innovation",description:"Captivate with inventiveness."},
+        PASSION:{label:"Passion",description:"Connect with emotion."},
+        POWER:{label:"Power",description:"Lead with assertiveness."},
+        PRESTIGE:{label:"Prestige",description:"Set higher standards."},
+        TRUST:{label:"Trust",description:"Earn respect by stability."},
+        CUNNING:{label:"Cunning",description:"Communicate meaningfully."},
+        KEEN:{label:"Keen",description:"Prevent with attention"}
+    },
+};
