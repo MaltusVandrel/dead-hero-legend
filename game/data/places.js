517a8b3cb9375bf10c5237f6fc8d923d0d9d1ec7
Maltus Vandrel <matgoben@gmail.com>
2020-10-18 21:05:58 -0300

    go

--- game/data/places.js
+++ game/data/places.js
@@ -2,4 +2,6 @@ var PLACES={
     _001START_1:{code:'001START_1'},
     _001START_2:{code:'001START_2'},
     _001START_3:{code:'001START_3'},
+    _001START_4:{code:'001START_4'},
+    _001BEACH_1:{code:'002BEACH_1'},
 };
--- game/main.js
+++ game/main.js
@@ -2,7 +2,6 @@
 
 var GAME={place:PLACES._001START_1,PLACES:{}};
 GAME.main={...ATRIBUTES};
-GAME.main.pointsToDistribute=6;
 //if(getStorage('save1')!=undefined&&getStorage('save1')!="undefined")GAME=fromJson(getStorage('save1'));
 setPlace(GAME.place);         
 
